313c3ef74f2709fc83181e369227c2484ba37d6c
Charu Sharma <charusharma1527@gmail.com>
2025-11-28 17:23:36 +0530

    floor using bs

--- search/src/interview/Floor.java
+++ search/src/interview/Floor.java
@@ -1,4 +1,26 @@
 package interview;
 
+// Return the index of largest no <=target
 public class Floor {
+    public static void main(String[] args){
+        int arr[]={2,5,8,9,17,89,899};
+        int target=18;
+        System.out.println(floor(arr,target));
+    }
+
+    static int floor(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+    }
 }
