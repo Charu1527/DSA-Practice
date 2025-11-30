4e4eb411288db46ac31b79f15503b101d0b13bc4
Charu Sharma <charusharma1527@gmail.com>
2025-11-30 14:23:41 +0530

    Peak Index in a Mountain Array

---
+++ search/src/interview/Mountain.java
@@ -0,0 +1,21 @@
+package interview;
+//852. Peak Index in a Mountain Array,,https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+public class Mountain {
+    public static void main(String[] args) {
+
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
