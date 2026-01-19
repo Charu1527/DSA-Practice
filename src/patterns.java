b0da760e7960f6f129a6fb674c0358f066b1ad5c
Charu Sharma <charusharma1527@gmail.com>
2026-01-19 18:00:35 +0530

    patterns

--- src/patterns.java
+++ src/patterns.java
@@ -5,26 +5,27 @@ public class patterns {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of rows : ");
         int n = input.nextInt();
-        pattern2(n);
+        pattern1(n);
     }
 
 
     static void pattern1(int n) {
-//        1st half of the code
-        for(int row=1;row<=n/2;row++){
-            for(int col=1;col<=row;col++){
-                System.out.print("* ");
+        for (int row = 1; row <= 2 * n - 1; row++) {
+
+            int totalColsInRow;
+            if (row <= n) {
+                totalColsInRow = row;          // 1st half
+            } else {
+                totalColsInRow = 2 * n - row;  // 2nd half
             }
-            System.out.println();
-        }
-//        2nd half of the code
-        for(int row=n/2;row<=n;row++){
-            for(int col=1;col<=n-row+1;col++){
+
+            for (int col = 1; col <= totalColsInRow; col++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
+
 
     static void pattern2(int n) {
         for(int row=1;row<=n;row++){
